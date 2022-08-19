294300ab0e9b2d80aa5c11b7a43cd9363f2956cb
Ranjith <ranjithstar256@gmail.com>
2022-08-19 18:29:18 +0530

    addded pause stop

--- app/src/main/java/com/e/marvel/MainActivity.java
+++ app/src/main/java/com/e/marvel/MainActivity.java
@@ -16,17 +16,28 @@ public class MainActivity extends AppCompatActivity {
 
     TextView tv;
     MediaPlayer m;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tv=findViewById(R.id.textView);
-         m = MediaPlayer.create(MainActivity.this,R.raw.kgf);
+        tv = findViewById(R.id.textView);
+        m = MediaPlayer.create(MainActivity.this, R.raw.kgf);
 
     }
 
     public void datpcr(View view) {
         m.start();
         Toast.makeText(this, "hi", Toast.LENGTH_SHORT).show();
+    }
+
+    public void pau(View view) {
+        m.pause();
+    }
+
+    public void stp(View view) {
+        m.stop();
+        m = MediaPlayer.create(MainActivity.this, R.raw.kgf);
+
     }
 }
